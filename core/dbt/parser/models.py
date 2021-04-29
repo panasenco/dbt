c1f412aebdcc93164afc0aa8681d7dd51e0a5da3
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-04-29 14:25:57 -0400

    draft of render_update override

--- core/dbt/parser/models.py
+++ core/dbt/parser/models.py
@@ -1,6 +1,7 @@
 from dbt.context.context_config import ContextConfig
 from dbt.contracts.graph.parsed import ParsedModelNode
 from dbt.dataclass_schema import ValidationError
+from dbt.dbt_jinja.compiler import extract_from_source
 from dbt.exceptions import CompilationException
 from dbt.node_types import NodeType
 from dbt.parser.base import IntermediateNode, SimpleSQLParser
@@ -24,10 +25,21 @@ class ModelParser(SimpleSQLParser[ParsedModelNode]):
     def render_update(
         self, node: IntermediateNode, config: ContextConfig
     ) -> None:
-        try:
-            self.render_with_context(node, config)
+        # run dbt-jinja extractor (powered by tree-sitter)
+        res = extract_from_source(node.raw_sql)
+        # if it didn't return an exception, fit the refs, sources, and configs
+        # into the node. Down the line the rest of the node will be updated with
+        # this information. (e.g. depends_on etc.)
+        if not isinstance(res, Exception):
+            for ref in res['refs']:
+                node.refs.append(ref)
+            for source in res['sources']:
+                # TODO change extractor to match type here
+                node.sources.append([source[0], source[1]])
+            for config in res['configs']:
+                node.config[config[0]] = config[1]
+
+            # TODO is this line necessary? not even sure what it does.
             self.update_parsed_node(node, config)
-        except ValidationError as exc:
-            # we got a ValidationError - probably bad types in config()
-            msg = validator_error_message(exc)
-            raise CompilationException(msg, node=node) from exc
+        else: 
+            super().render_update(node, config)
